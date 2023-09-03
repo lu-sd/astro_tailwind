c14e557ec3559f0c7903971aca189b055675ceb3
lu-sd <luhou.me@gmail.com>
2023-09-03 11:20:27 -0700

    use Shadcn input

--- src/components/InputWithButton.tsx
+++ src/components/InputWithButton.tsx
@@ -4,9 +4,11 @@ import { Input } from '@/components/ui/input'
 export function InputWithButton() {
   return (
     // <div className="mt-4 flex flex-col   space-x-2 space-y-4 md:flex-row md:items-center">
-    <div className="flex w-full max-w-sm items-center space-x-2 space-y-0">
+    <div className=" mt-2 flex  flex-col  space-y-3 md:flex-row md:space-x-2 md:space-y-0">
       <Input type="email" placeholder="Email" />
-      <Button type="submit">Subscribe</Button>
+      <Button className="flex" type="submit">
+        Subscribe
+      </Button>
     </div>
   )
 }
